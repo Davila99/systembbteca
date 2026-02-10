9f97a4546b0fc880b97481e96c36c659045b3cb2
eliseo <davilaeliseo453@gmail.com>
2026-02-09 21:20:59 -0600

    Update settings.py

--- systembbteca/settings.py
+++ systembbteca/settings.py
@@ -78,7 +78,7 @@ WSGI_APPLICATION = 'systembbteca.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'dbteca',
+        'NAME': 'dbeliseo',
         'USER': 'root',
         'PASSWORD': 'root',
         'HOST': '127.0.0.1', 
